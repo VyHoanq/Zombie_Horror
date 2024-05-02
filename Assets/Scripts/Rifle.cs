2bb4502714dc1ad994c10543c10e381cb906bb9b
VyHoanq <104727859+VyHoanq@users.noreply.github.com>
2024-05-02 18:23:23 +0700

    Project Files

---
+++ Assets/Scripts/Rifle.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rifle : MonoBehaviour
+{
+    [Header("Rifle Things")]
+    public Camera cam;
+
+    public float giveDamageOf = 10f;
+    public float shootingRange = 100f;
+    public float fireChange = 15f;
+    private float nextTimeToShoot = 0f;
+    public PlayerScript player;
+    public Transform hand;
+
+    [Header("Rifle Ammunition and shooting")]
+    private int maximumAmmunition = 32;
+    public int mag = 10;
+    private int presentAmmunition;
+    public float reloadingTime = 1.3f;
+    private bool setReloading = false;
+
+
+    [Header("Rifle Effects")]
+    public ParticleSystem muzzleSpark;
+    public GameObject WoodedEffect;
+
+    private void Awake()
+    {
+        transform.SetParent(hand);
+        presentAmmunition = maximumAmmunition;
+    }
+
+
+    private void Update()
+    {
+        if(setReloading)
+        return;
+
+        if(presentAmmunition <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToShoot)
+
+        {
+            nextTimeToShoot = Time.time + 1f / fireChange;
+            Shoot();
+        }
+    }
+    private void Shoot()
+    {
+        // check for mag 
+        if(mag ==0)
+        {
+            // show ammo out next
+            return;
+        }
+        presentAmmunition--;
+        if(presentAmmunition == 0)
+        {
+            mag--;
+        }
+
+        //updating the UI
+
+        muzzleSpark.Play();
+        RaycastHit hitInfo;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hitInfo, shootingRange))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            ObjectToHit objectToHit = hitInfo.transform.GetComponent<ObjectToHit>();
+
+
+            if (objectToHit != null)
+            {
+                objectToHit.ObjectHitDamage(giveDamageOf);
+                GameObject WoodGo = Instantiate(WoodedEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+                Destroy(WoodGo, 1f);
+            }
+        }
+    }
+    IEnumerator Reload()
+    {
+        player.playerSpeed = 0f;
+        player.playerSprint = 0f;
+        setReloading = true;
+        Debug.Log("Reloading...");
+        //play aim
+        //play reload sound
+        yield return new WaitForSeconds(reloadingTime);
+        //play aim
+        presentAmmunition = maximumAmmunition;
+        player.playerSpeed = 1.9f;
+        player.playerSprint = 3;
+        setReloading = false;
+    }
+}
